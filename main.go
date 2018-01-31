531943c864f62678ff51aafc1208c182d66b21ef
midorigreen <stitow14@gmail.com>
2018-01-31 20:06:43 +0900

    Add print server start

--- main.go
+++ main.go
@@ -8,6 +8,8 @@ import (
 	"github.com/midorigreen/goapi-architecture/handler"
 )
 
+const port = ":3333"
+
 func main() {
 	if err := run(); err != nil {
 		log.Fatalln(err)
@@ -17,5 +19,6 @@ func main() {
 func run() error {
 	r := chi.NewRouter()
 	handler.Registe(r)
+	log.Printf("Server Start Port%s", port)
 	return http.ListenAndServe(":3333", r)
 }
